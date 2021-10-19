149446ad999705a6418740c8b32675f8334edb4a
Женечек <53404220+J3ne4ek@users.noreply.github.com>
2021-10-19 11:50:24 +0300

    utils change

--- parsers/utils.py
+++ parsers/utils.py
@@ -3,3 +3,8 @@
 def wildberries_product_to_object(product_name, current_price, old_price, img_url):
     return {'product_name': product_name, 'current_price': current_price, 'old_price': old_price, 'img': img_url}
 
+def cian_apart_to_object(name, price, location):
+    return {'name': name, 'price': price, 'location' : location}
+    
+def habr_article_to_object(times, tags, habs, saved):
+    return {'times' : times, 'tags': tags, 'habs' : habs, 'saved' : saved}
