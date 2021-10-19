651eed53fe2c4f171b2f4c06e4e3fdb09f2bcee7
Alexandra <alexandraroze2000@gmail.com>
2021-10-19 20:06:15 +0300

    utils fixed

--- parsers/utils.py
+++ parsers/utils.py
@@ -8,10 +8,14 @@ def write_to_json(object, path):
     file.close()
 
 def wildberries_product_to_object(product_name, current_price, old_price, img_url):
-    return {'product_name': product_name, 'current_price': current_price, 'old_price': old_price, 'img': img_url}
+    object = {'product_name': product_name, 'current_price': current_price, 'old_price': old_price, 'img': img_url}
+    path = 'wildberries_data'
+    write_to_json(object, path)
 
 def cian_apart_to_object(name, price, location):
-    return {'name': name, 'price': price, 'location' : location}
+    object = {'name': name, 'price': price, 'location' : location}
+    path = 'cian_data'
+    write_to_json(object, path)
     
 def habr_article_to_object(times, tags, habs, saved):
     object = {'times' : times, 'tags': tags, 'habs' : habs, 'saved' : saved}
