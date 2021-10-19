98e07310c6fe375d1581aa2de1eb862603ad0c7e
Женечек <53404220+J3ne4ek@users.noreply.github.com>
2021-10-19 11:54:55 +0300

    cian_parser add

---
+++ parsers/bs4/cian_parser.py
@@ -0,0 +1,11 @@
+from parsers.utils import cian_apart_to_object
+from bs4 import BeautifulSoup
+
+def parse_cian_html(html_source):
+    response = BeautifulSoup(html_source, 'html.parser')
+    cards = response.select(r'[data-name="CardComponent"]')
+    for card in cards:
+        name = card.select_one(r'[data-name="TitleComponent"]').text.strip()
+        price = card.select_one(r'[data-mark="MainPrice"]').text.strip()
+        location = ','.join([x.text.strip() for x in card.select(r'[data-name="GeoLabel"]')])
+        yield cian_apart_to_object(name, price, location)
